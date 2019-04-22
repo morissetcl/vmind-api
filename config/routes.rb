ab7ab6a364eccf48092b46b2aabe2a3ba19579c1
Clement Morisset <morissetcl87@gmail.com>
2019-04-22 23:00:51 +0200

    add privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  match '/privacy_policy', to: 'static_pages#privacy_policy', via: 'get', as: :help
+
   namespace :api do
     namespace :v1 do
       resources :verses
