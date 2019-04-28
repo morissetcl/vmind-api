8d35c520fa33f4a083e450abf3ca335ccfa2cfa1
Clement Morisset <morissetcl87@gmail.com>
2019-04-28 21:03:03 +0200

    select

--- app/admin/verse.rb
+++ app/admin/verse.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Verse do
   permit_params :need, :theme, :name, :content
+
+  form do |f|
+    f.inputs do
+      f.input :need, as: :select, collection: ['Love', 'Forgiveness', 'Faith', 'Health', 'Praises', 'Courage']
+      f.input :theme
+      f.input :name
+      f.input :content
+    end
+    f.actions
+  end
 end
