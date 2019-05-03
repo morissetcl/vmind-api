72a9e23556843862c91e783e23462acecbee1a3d
Clement Morisset <morissetcl87@gmail.com>
2019-05-03 15:16:20 +0200

    delete comment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,8 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
