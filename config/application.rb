1e68997d4d63bb8f24c1155d143e44ec3f0ea3fd
Clement Morisset <morissetcl87@gmail.com>
2023-01-03 09:20:22 -0500

    Use rails 7

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ Bundler.require(*Rails.groups)
 module VmindApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
   end
 end
